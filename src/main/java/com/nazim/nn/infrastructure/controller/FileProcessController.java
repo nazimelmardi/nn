64848f6f9b2abfb7106dd38f0aa42c715b27a520
Nazim El Mardi <nazim.elmardi@googlemail.com>
2020-11-06 02:30:05 +0100

    controller basics added

---
+++ src/main/java/com/nazim/nn/infrastructure/controller/FileProcessController.java
@@ -0,0 +1,11 @@
+package com.nazim.nn.infrastructure.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import com.nazim.nn.infrastructure.api.NnApi;
+
+
+@RestController
+@RequiredArgsConstructor
+public class FileProcessController implements NnApi {
+}
